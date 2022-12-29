41cb6f47472961c1ec56fc4236e85a576dce2c59
Francisco Fernandes <francisco201262@gmail.com>
2022-12-29 00:49:46 +0000

    small ui fixes

---
+++ Market/app/src/main/java/com/example/market/ui/fragments/MessagesFragment.java
@@ -0,0 +1,11 @@
+package com.example.market.ui.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.market.data.MarketViewModel;
+import com.example.market.databinding.FragmentMessagesBinding;
+
+public class MessagesFragment extends Fragment {
+    private FragmentMessagesBinding binding;
+    private MarketViewModel viewModel;
+}
