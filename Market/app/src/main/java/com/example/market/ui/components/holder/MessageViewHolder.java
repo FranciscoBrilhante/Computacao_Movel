263a60ba894ae4f4d0053b467f4844e41fae57f7
José Esperança <43673761+jpmesperanca@users.noreply.github.com>
2023-01-05 02:54:22 +0000

    Fix mensagens estranhas

--- Market/app/src/main/java/com/example/market/ui/components/holder/MessageViewHolder.java
+++ Market/app/src/main/java/com/example/market/ui/components/holder/MessageViewHolder.java
@@ -30,24 +30,26 @@ public class MessageViewHolder extends RecyclerView.ViewHolder {
 
     public void bind(Message message) {
         this.message = message;
+        float scale = content.getContext().getResources().getDisplayMetrics().density;
+        int size2 = (int) (8*scale + 0.5f);
+        int size4 = (int) (12*scale + 0.5f);
+
+        LinearLayout.LayoutParams params=(LinearLayout.LayoutParams) content.getLayoutParams();
+
         if (message.getSender()==profileID) {
-            LinearLayout.LayoutParams params=(LinearLayout.LayoutParams) content.getLayoutParams();
-            params.gravity = Gravity.END|Gravity.TOP;
+            params.gravity = Gravity.END;
             content.setLayoutParams(params);
             content.setBackgroundResource(R.drawable.bubble_outcoming);
-            float scale = content.getContext().getResources().getDisplayMetrics().density;
             int size1 = (int) (25*scale + 0.5f);
-            int size2 = (int) (8*scale + 0.5f);
             int size3 = (int) (17*scale + 0.5f);
-            int size4 = (int) (12*scale + 0.5f);
             content.setPadding(size3,size2,size1,size4);
         }
         else{
-            float scale = content.getContext().getResources().getDisplayMetrics().density;
+            params.gravity = Gravity.START;
+            content.setLayoutParams(params);
+            content.setBackgroundResource(R.drawable.bubble_incoming);
             int size1 = (int) (17*scale + 0.5f);
-            int size2 = (int) (8*scale + 0.5f);
             int size3 = (int) (25*scale + 0.5f);
-            int size4 = (int) (12*scale + 0.5f);
             content.setPadding(size3,size2,size1,size4);
         }
         content.setText(message.getContent());
